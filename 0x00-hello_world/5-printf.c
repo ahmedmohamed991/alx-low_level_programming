927fa70a7ce0728dc9f1ef03defabfc2478fc7a9
ahmed hafez <ahmmeedhafez22@gmail.com>
2023-08-10 17:14:59 -0700

    aa

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,13 +1,9 @@
 #include<stdio.h>
-
 /**
- * main - Entry point
- *
- * Description : A c program that prints with printf function
- *
- * Return: Always 0 (success)
-*/ 
-
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
