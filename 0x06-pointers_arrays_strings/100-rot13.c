74c60caed763098b82719410d2cd0240206c056a
ahmed hafez <ahmmeedhafez22@gmail.com>
2023-08-24 20:03:51 +0300

    ahmed

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * rot13 - encoder rot13
+ * @s: pointer to string params
+ *
+ * Return: *s
+ */
+
+char *rot13(char *s)
+{
+	int a;
+	int h;
+	char data1[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char datarot[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	for (a = 0; s[a] != '\0'; a++)
+	{
+		for (h = 0; h < 52; h++)
+		{
+			if (s[a] == data1[h])
+			{
+				s[a] = datarot[h];
+				break;
+			}
+		}
+	}
+	return (s);
+}
