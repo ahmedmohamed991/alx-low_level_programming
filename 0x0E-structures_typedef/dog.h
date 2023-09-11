6be1ff292bf1d36d4fe4002ffaf763e42706fa2d
ahmed hafez <ahmmeedhafez22@gmail.com>
2023-09-12 01:10:08 +0300

    ahmed

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,6 @@
 #ifndef DOG_H
 #define DOG_H
+
 /**
 * struct dog - a dog's information
 * @name: First member
@@ -13,5 +14,5 @@ struct dog {
 	char *name;
 	float age;
 	char *owner;
-}
+};
 #endif
